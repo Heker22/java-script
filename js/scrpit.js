e7da1c094289e94399c80db7539cb2fd9244eeb8
Heker22 <146105661+Heker22@users.noreply.github.com>
2024-11-12 15:34:54 +0100

    s

--- js/scrpit.js
+++ js/scrpit.js
@@ -1,3 +1,24 @@
 "use strict"
 
+
 console.log("Hello Matviy")
+
+
+/*
+a=b*2
+a-b= sminni
+=-*= operators
+*/
+
+let b = 7;
+const a = b * 2;
+console.log(a);
+
+const age = 13;
+const name = "Matviy"
+console.log("Hello my name is " + name +" i'm " + age + " years old. " );
+
+console.log("i love java-script".length);
+console.log("i love java-script".toUpperCase());
+let firstName; 
+console.log( firstName);
