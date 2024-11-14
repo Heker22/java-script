a6f084d6cc8d0379e1a71ca4ad2fab5ac4059904
Heker22 <146105661+Heker22@users.noreply.github.com>
2024-11-14 15:57:14 +0100

    1

--- js/scrpit.js
+++ js/scrpit.js
@@ -10,15 +10,59 @@ a-b= sminni
 =-*= operators
 */
 
-let b = 7;
-const a = b * 2;
+/*
+let b = 2024;
+const a = b - 102;
 console.log(a);
 
+
 const age = 13;
-const name = "Matviy"
-console.log("Hello my name is " + name +" i'm " + age + " years old. " );
+const namee = "Matviy"
+console.log("Hello my name is " + namee +" i'm " + age + " years old. " );
 
 console.log("i love java-script".length);
 console.log("i love java-script".toUpperCase());
 let firstName; 
 console.log( firstName);
+console.log(typeof age);
+console.log(typeof namee);
+console.log(typeof a);
+
+const message = 'Hello user';
+alert(message);
+const newMessage = confirm('please confirm hotel reservatio') 
+console.log(newMessage)
+
+
+const newAge = prompt('How old ARE YOU?');
+console.log(newAge)
+*/
+
+/*
+const hi = 'Привіт';
+console.log( typeof hi );
+
+const numb = 123;
+console.log(typeof numb);
+
+const verificate = true;
+console.log(typeof verificate);
+
+const text = 'True';
+console.log( typeof text );
+*/
+
+const age2 = 12;
+const age3 = 12;
+const total = age2 + age3
+console.log(total)
+
+const age4 = 7;
+const age5 = 3;
+const total2 = age4 * age5;
+console.log(total2)
+
+
+const namee = 'Mac Book Pro 13';
+const price = 2000;
+console.log('Name: ', namee, ', price: ', price);
