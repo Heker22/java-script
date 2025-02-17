7a822d996e4af227cac5a90369a950a634b4a9f0
Heker22 <146105661+Heker22@users.noreply.github.com>
2025-02-17 15:38:15 +0100

    anita

--- js/scrpit.js
+++ js/scrpit.js
@@ -1412,11 +1412,11 @@ console.log(max(array));
 
 const logItems = function (array) {
 
-    for (let i = 0; i < array.length; i++){
+    for (let i = 0; i < array.length; i++) {
         console.log(`${i + 1}  - ${array[i]}  `)
     }
 
-    
+
 };
 
 /*
@@ -1431,17 +1431,126 @@ logItems([5, 10, 15, 20, 25, 30, 35, 40, 45, 50]);
 
 
 Напиши скрипт підрахунку вартості гравіювання прикрас. Для цього створи функцію calculateEngravingPrice(message, pricePerWord) приймаючу рядок (в рядку будуть тільки слова і прогалини) і ціну гравіювання одного слова, і повертає ціну гравіювання всіх слів в рядку.
-*/
+
 const calculateEngravingPrice = function (message, pricePerWord ) {
   
+    const word = message.split(' ') ;
+
+    return word.length * pricePerWord  ;
+    
 };
 
 
  
- 
+ /*
 console.log(
     calculateEngravingPrice(
       'Proin sociis natoque et magnis parturient montes mus',
       10,
     ),
   ); // 80
+
+
+  console.log(
+    calculateEngravingPrice(
+      'sidh sdiqhwds dwuoh sdqiho',
+      89,
+    ),
+  ); 
+*/
+/*practice
+
+
+Напиши функцію findLongestWord(string), яка приймає параметром довільний рядок (в рядку будуть тільки слова і прогалини) і повертає найдовше слово в цьому рядку.
+
+
+
+
+const findLongestWord = function (string) {
+    const words = string.split(' ');
+    let first = words[0];
+
+    for (let i = 0; i < words.length; i++) {
+
+        if (words[i].length > first.length) {
+
+            first = words[i];
+        }
+    }
+
+    return first;
+
+};
+/*
+ * Виклич функції для перевірки працездатності твоєї реалізації.
+ 
+console.log(findLongestWord('The quick brown fox jumped over the lazy dog')); // 'jumped'
+
+console.log(findLongestWord('Google do a roll')); // 'Google'
+
+console.log(findLongestWord('May the force be with you')); // 'force'
+
+*/
+
+
+/*2
+
+
+Напиши функцію formatString(string) яка приймає рядок і форматує його якщо необхідно.
+
+Якщо довжина рядка не перевищує 40 символів, функція повертає її в початковому   вигляді.
+Якщо довжина більше 40 символів, то функція обрізає рядок до 40-ка символів і додає в кінець рядка три крапки '...', після чого повертає   укорочену версію.
+
+
+
+const formatString = function (string) {
+
+    if (string.length < 40) {
+
+        return string;
+    }
+
+    return string.slice(0, 40) + '...' ;
+};
+
+
+ * Виклич функції для перевірки працездатності твоєї реалізації.
+ 
+console.log(formatString('Curabitur ligula sapien, tincidunt non.'));
+// повернеться оригінальний рядок
+
+console.log(formatString('Vestibulum facilisis, purus nec pulvinar iaculis.'));
+// повернеться форматований рядок
+
+console.log(formatString('Curabitur ligula sapien.'));
+// повернеться оригінальний рядок
+
+console.log(
+    formatString(
+        'Nunc sed turpis. Curabitur a felis in nunc fringilla tristique.',
+    ),
+);
+// повернеться форматований рядок
+
+
+/*3
+
+Напиши функцію checkForSpam(message), приймаючу 1 параметр message - рядок. Функція перевіряє її на вміст слів spam і sale. Якщо знайшли заборонене слово, то функція повертає true, якщо заборонених слів немає функція повертає false. Слова в рядку можуть бути в довільному регістрі.
+*/
+const checkForSpam = function (message) {
+  
+    const frase = message.toLowerCase();
+    
+    return frase.includes('spam') || frase.includes('sale')
+};
+
+/*
+ * Викличи функції для перевірки працездатності твоєї реалізації.
+ */
+console.log(checkForSpam('Latest technology news')); // false
+
+console.log(checkForSpam('JavaScript weekly newsletter')); // false
+
+console.log(checkForSpam('Get best sale offers now!')); // true
+
+console.log(checkForSpam('[SPAM] How to earn fast money?')); // true
